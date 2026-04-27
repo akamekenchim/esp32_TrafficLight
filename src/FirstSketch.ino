77d936baabd075b4e1428e77de81a0d652a797c8
KenChim <giaanhnp@gmail.com>
2026-04-27 18:40:08 +0700

    Added a pedestrian light system in sync with Traffic lights

--- src/FirstSketch.ino
+++ src/FirstSketch.ino
@@ -3,23 +3,28 @@ const int GREEN_LIGHT = 27;
 const int YELLOW_LIGHT = 26;
 const int RED_LIGHT = 25;
 const int SWITCH_PIN = 33;
+const int PED_GREEN = 16;
+const int PED_RED = 17;
 typedef struct L{
   uint8_t pin;
   uint16_t duration;
   uint8_t blinkCount;
 } Light;
 uint8_t blinkCounter = 0;
-uint8_t curLight = 0;
 bool lastState = LOW;
 Light tLights[] = { {GREEN_LIGHT, 3000, 1}, {YELLOW_LIGHT, 400, 3}, {RED_LIGHT, 3000, 1}};
-
-
+typedef enum TS {GREEN, YELLOW, RED} TrafficState;
+TrafficState curLight = GREEN;
 void setup(){
   pinMode(GREEN_LIGHT, OUTPUT);
   pinMode(YELLOW_LIGHT, OUTPUT);
   pinMode(RED_LIGHT, OUTPUT);
-  //pinMode(SWITCH_PIN, INPUT_PULLDOWN);
+  pinMode(PED_RED, OUTPUT);
+  pinMode(PED_GREEN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT_PULLDOWN);
   digitalWrite(tLights[curLight].pin, HIGH);
+  digitalWrite(PED_RED, HIGH);
+  
 }
 void toggle(uint8_t index){
   if(digitalRead(tLights[index].pin) == HIGH){
@@ -35,25 +40,36 @@ void loop(){
   if(curState == HIGH && lastState == LOW){
     lastTime = curTime;
     digitalWrite(tLights[curLight].pin, LOW);
-    curLight = 2;
+    curLight = RED;
     digitalWrite(tLights[curLight].pin, HIGH);
     blinkCounter = 0;
+    digitalWrite(PED_RED, LOW);
+    digitalWrite(PED_GREEN, HIGH);
   } 
   lastState = curState;
-  if(curTime - lastTime >= tLights[curLight].duration){
-    if(tLights[curLight].blinkCount > 1 && blinkCounter <= tLights[curLight].blinkCount * 2){
-      toggle(curLight);
-      lastTime = curTime;
-      blinkCounter++;
-    }
-    else{
-      digitalWrite(tLights[curLight].pin, LOW);
-      curLight++;
-      if(curLight > 2) curLight = 0;
-      lastTime = curTime;
-      digitalWrite(tLights[curLight].pin, HIGH);  
-      blinkCounter = 0;
-    } 
-  }
 
+  if(curTime - lastTime >= tLights[curLight].duration){
+    lastTime = curTime;
+    if(curLight != YELLOW) digitalWrite(tLights[curLight].pin, LOW);
+    if(curLight == GREEN) curLight = YELLOW;
+    else if(curLight == YELLOW && blinkCounter == 6){
+      blinkCounter = 0;
+      curLight = RED;
+    }
+    else if(curLight == RED) curLight = GREEN;
+    switch(curLight){
+      case RED:
+        digitalWrite(RED_LIGHT, HIGH);
+        break;
+      case YELLOW:
+        toggle(YELLOW);
+        blinkCounter++;
+        break;
+      case GREEN:
+        digitalWrite(GREEN_LIGHT, HIGH);
+        break;
+    }
+    digitalWrite(PED_RED, (curLight == GREEN) || (curLight == YELLOW));
+    digitalWrite(PED_GREEN, (curLight == RED));
+  }
 }
